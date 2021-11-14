c1cabafe03e52c5603a696c84651069a7c3ac8b5
jefishub <jere.manni@gmail.com>
2021-11-14 19:11:40 +0200

    working calculator

--- App.js
+++ App.js
@@ -1,12 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const [value1, setValue1] = useState(0);
+  const [value2, setValue2] = useState(0);
+  const [result, setResult] = useState(value1 + value2);
+
+  const addition = () => { setResult(value1 + value2); }
+  const subtraction = () => { setResult(value1 - value2); }
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <View style={{}}>
+        <Text>Result: {result}</Text>
+        <TextInput
+          keyboardType='numeric'
+          style={{
+            width: 200,
+            borderColor: 'gray',
+            borderWidth: 1
+          }} onChangeText={value1 => setValue1(Number(value1))} value={value1} />
+        <TextInput
+          keyboardType='numeric' style={{
+            width: 200,
+            borderColor: 'gray',
+            borderWidth: 1
+          }} onChangeText={value2 => setValue2(Number(value2))} value={value2} />
+        <StatusBar style="auto" />
+      </View>
+      <View style={{flexDirection: 'row'}}>
+        <Button onPress={addition} title="+" />
+        <View style={{marginRight: 10 }}></View>
+        <Button onPress={subtraction} title="-" />
+      </View>
     </View>
   );
 }
