8c093b888db9bdac8e97a1b1b85b72626b9ccd80
jefishub <jere.manni@gmail.com>
2021-11-15 21:52:43 +0200

    Exercise Recipe Finder

--- Calculator.js
+++ Calculator.js
@@ -1,49 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, FlatList, Image } from 'react-native';
 
-export default function Calculator({navigation}) {
-  const [value1, setValue1] = useState(0);
-  const [value2, setValue2] = useState(0);
-  const [result, setResult] = useState(value1 + value2);
-  const [data, setData] = useState([]);
+export default function Calculator({ navigation }) {
+  const [keyword, setKeyword] = useState('');
+  const [repositories, setRepositories] = useState([]);
 
-  const addition = () => {
-    setData([...data, value1 + " + " + value2 + " = " + (value1 + value2)]);
-    setResult(value1 + value2);
-  }
-  const subtraction = () => {
-    setData([...data, value1 + " - " + value2 + " = " + (value1 - value2)]);
-    setResult(value1 - value2);
+
+  const getRepositories = (keyword) => {
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${keyword}`)
+      .then(response => response.json())
+      .then(responseJson => setRepositories(responseJson.meals))
+      .catch(error => { Alert.alert('Error', error); });
   }
 
   return (
     <View style={styles.container}>
-      <View>
-        <Text>Result: {result}</Text>
-        <TextInput
-          keyboardType='numeric'
-          style={{
-            width: 200,
-            borderColor: 'gray',
-            borderWidth: 1
-          }} onChangeText={value1 => setValue1(Number(value1))} value={value1} />
-        <TextInput
-          keyboardType='numeric' style={{
-            width: 200,
-            borderColor: 'gray',
-            borderWidth: 1
-          }} onChangeText={value2 => setValue2(Number(value2))} value={value2} />
-        <StatusBar style="auto" />
-      </View>
-      <View style={{ flexDirection: 'row' }}>
-        <Button onPress={addition} title="+" />
-        <View style={{ marginRight: 10 }}></View>
-        <Button onPress={subtraction} title="-" />
-        <View style={{ marginRight: 10 }}></View>
-        <Button onPress={() => navigation.navigate('History', {history: data})} title="History"/>
-      </View>
-    </View>
+      <TextInput style={{
+        fontSize: 18,
+        width: 200
+      }} placeholder='keyword' onChangeText={text => setKeyword(text)} />
+      <Button title="Find" onPress={() => getRepositories(keyword)} />
+      <FlatList style={{ marginLeft: "5%" }}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) =>
+          <View>
+            <Text style={{ fontSize: 18, fontWeight: "bold" }}>{item.full_name}</Text>
+            <Text style={{ fontSize: 16 }}> {item.strMeal}</Text>
+            <Image
+              style={styles.tinyLogo}
+              source={{uri: item.strMealThumb}}
+            />
+          </View >}
+        data={repositories} />
+    </View >
   );
 }
 
@@ -53,5 +43,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  tinyLogo: {
+    width: 50,
+    height: 50,
   },
 });
