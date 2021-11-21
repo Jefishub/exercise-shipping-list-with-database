6f84f926d07396c9cc6eafa2c24fc88bd729f681
jefishub <jere.manni@gmail.com>
2021-11-21 18:38:46 +0200

    base example

--- EuroConverter.js
+++ EuroConverter.js
@@ -1,74 +1,46 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, Button, TextInput, Alert, FlatList, Image } from 'react-native';
-import { Picker } from '@react-native-picker/picker';
-
-const API_KEY = "99e601a5850683313a9407c69eef229d"
-
-export default function EuroConverter({ navigation }) {
-  const [selected, setSelected] = useState('');
-  const [rates, setRates] = useState([]);
-  const [amount, setAmount] = useState(1);
-  const [eur, setEur] = useState('Press button to convert');
-
-  useEffect(() => {
-    getRates(API_KEY);
-  }, [])
-
-  useEffect(() => {
-    setEur('Press button to convert')
-  }, [selected, amount])
-
-  const convert = () => {
-    const amountEur = Number(amount) / rates[selected]
-    setEur(`${amountEur.toFixed(2)}eur`);
-  }
-
-  const getRates = (api_key) => {
-    fetch(`http://api.exchangeratesapi.io/latest?access_key=${api_key}`)
-      .then(response => response.json())
-      .then(responseJson => setRates(responseJson.rates))
-      .catch(error => { Alert.alert('Error', error); });
-  }
-
-  return (
-    <View style={styles.container}>
-      <Image style={{ width: 200, height: 200 }}
-        source={require('./eurocoin.png')}
-      />
-      <Text
-        style={{ borderBottomWidth: 1, borderColor: "#ababab", backgroundColor: '#ffff11' }}>{eur}</Text>
-      <View style={{ flexDirection: 'row' }}>
-        <TextInput
-          style={{ borderBottomWidth: 1, borderColor: "#ababab" }}
-          placeholder={'Amount'}
-          keyboardType={'numeric'}
-          value={amount}
-          onChangeText={text => setAmount(text)}
-        />
-        <Picker
-          selectedValue={selected}
-          style={{ height: 50, width: 100 }}
-          onValueChange={(itemValue, itemIndex) => {
-            console.log(itemIndex, itemValue);
-            setSelected(itemValue);
-          }}>
-          {Object.keys(rates).map(key => (<Picker.Item label={key} value={key} key={key} />))}
-        </Picker>
+  import React from 'react';
+  import { StyleSheet, StatusBar, View } from 'react-native';
+  import MapView, { Marker } from 'react-native-maps';
+  
+  export default function App() {
+    const region = {
+      latitude: 60.200692,
+      longitude: 24.934302,
+      latitudeDelta: 0.0322,
+      longitudeDelta: 0.0221
+    };
+  
+    const coordinates = {
+      latitude: 60.201373,
+      longitude: 24.934041
+    };
+  
+    return (
+      <View style={styles.container}>
+        <MapView
+          style={styles.map}
+          initialRegion={region}
+        >
+          <Marker
+            coordinate={coordinates}
+            title='Haaga-Helia'
+          />
+        </MapView>
       </View>
-      <Button title="Convert" onPress={convert} />
-    </View >
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  tinyLogo: {
-    width: 50,
-    height: 50,
-  },
-});
+    );
+  }
+  
+  const styles = StyleSheet.create({
+    container: {
+      paddingTop:StatusBar.currentHeight,
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    map: {
+      flex: 1,
+      width: "100%",
+      height: "100%"
+    }
+  });
