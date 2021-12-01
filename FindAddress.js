860bab0e6936f257ee591859dbdcd8a47716aafe
jefishub <jere.manni@gmail.com>
2021-12-01 20:32:38 +0200

    find with location

--- FindAddress.js
+++ FindAddress.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, StatusBar, View, TextInput, Button } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
+import * as Location from 'expo-location';
 
 const API_URL = "http://www.mapquestapi.com/geocoding/v1/address";
 const API_KEY = "nk4AOXVkJGl4bHJ7ycsAQdTN2JRd4YW1";
@@ -10,7 +11,29 @@ const INITIAL_MARKER = { latitude: 60.200692, longitude: 24.934302 }
 export default function App() {
   const [latLng, setLatLng] = useState(INITIAL_REGION);
   const [marker, setMarker] = useState(INITIAL_MARKER);
-  const [locationAddress, setLocationAddress] = useState('Helsinki');
+  const [locationAddress, setLocationAddress] = useState('Current Location');
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        Alert.alert('No permissionto get location');
+        return;
+      }
+      const location = await Location.getCurrentPositionAsync({});
+      setLatLng(
+        {
+          latitude: location.coords.latitude,
+          longitude: location.coords.longitude,
+          latitudeDelta: 0.003,
+          longitudeDelta: 0.002,
+        });
+      setMarker({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude
+      })
+    })();
+  }, []);
 
   const fetchData = () => {
     fetch(API_URL + "?key=" + API_KEY + "&location=" + locationAddress, { method: 'GET' })
