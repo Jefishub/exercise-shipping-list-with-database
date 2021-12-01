4c54e0aeb7abe94dc9cec6c39e968b8f271d88fa
jefishub <jere.manni@gmail.com>
2021-12-01 21:38:28 +0200

    works!

--- App.js
+++ App.js
@@ -2,42 +2,42 @@ import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, TextInput, View, Button, FlatList } from 'react-native';
 import * as SQLite from 'expo-sqlite';
 
-const db = SQLite.openDatabase('coursedb.db');
+const db = SQLite.openDatabase('shopping_listdb.db');
 
 export default function App() {
-  const [credit, setCredit] = useState('');
-  const [title, setTitle] = useState('');
-  const [courses, setCourses] = useState([]);
+  const [amount, setAmount] = useState('');
+  const [product, setProduct] = useState('');
+  const [items, setItems] = useState([]);
 
   useEffect(() => {
     db.transaction(tx => {
-      tx.executeSql('create table if not exists course (id integer primary key not null, credits int, title text);');
+      tx.executeSql('create table if not exists shopping_list (id integer primary key not null, amounts text, product text);');
     });
     updateList();
   }, []);
 
-  // Save course
+  // Save shopping_list
   const saveItem = () => {
     db.transaction(tx => {
-      tx.executeSql('insert into course (credits, title) values (?, ?);', [parseInt(credit), title]);
+      tx.executeSql('insert into shopping_list (amounts, product) values (?, ?);', [amount, product]);
     }, null, updateList
     )
   }
 
-  // Update courselist
+  // Update shopping_list list
   const updateList = () => {
     db.transaction(tx => {
-      tx.executeSql('select * from course;', [], (_, { rows }) =>
-        setCourses(rows._array)
+      tx.executeSql('select * from shopping_list;', [], (_, { rows }) =>
+        setItems(rows._array)
       );
     });
   }
 
-  // Delete course
+  // Delete shopping_list
   const deleteItem = (id) => {
     db.transaction(
       tx => {
-        tx.executeSql(`delete from course where id = ?;`, [id]);
+        tx.executeSql(`delete from shopping_list where id = ?;`, [id]);
       }, null, updateList
     )
   }
@@ -57,20 +57,20 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <TextInput placeholder='Title' style={{ marginTop: 30, fontSize: 18, width: 200, borderColor: 'gray', borderWidth: 1 }}
-        onChangeText={(title) => setTitle(title)}
-        value={title} />
-      <TextInput placeholder='Credits' keyboardType="numeric" style={{ marginTop: 5, marginBottom: 5, fontSize: 18, width: 200, borderColor: 'gray', borderWidth: 1 }}
-        onChangeText={(credit) => setCredit(credit)}
-        value={credit} />
+      <TextInput placeholder='Product' style={{ marginTop: 30, fontSize: 18, width: 200, borderColor: 'gray', borderWidth: 1 }}
+        onChangeText={(product) => setProduct(product)}
+        value={product} />
+      <TextInput placeholder='Amounts' keyboardType="numeric" style={{ marginTop: 5, marginBottom: 5, fontSize: 18, width: 200, borderColor: 'gray', borderWidth: 1 }}
+        onChangeText={(amount) => setAmount(amount)}
+        value={amount} />
       <Button onPress={saveItem} title="Save" />
-      <Text style={{ marginTop: 30, fontSize: 20 }}>Courses</Text>
+      <Text style={{ marginTop: 30, fontSize: 20 }}>Items</Text>
       <FlatList
         style={{ marginLeft: "5%" }}
         keyExtractor={item => item.id.toString()}
-        renderItem={({ item }) => <View style={styles.listcontainer}><Text style={{ fontSize: 18 }}>{item.title}, {item.credits}</Text>
-          <Text style={{ fontSize: 18, color: '#0000ff' }} onPress={() => deleteItem(item.id)}> Done</Text></View>}
-        data={courses}
+        renderItem={({ item }) => <View style={styles.listcontainer}><Text style={{ fontSize: 18 }}>{item.product}, {item.amounts}</Text>
+          <Text style={{ fontSize: 18, color: '#0000ff' }} onPress={() => deleteItem(item.id)}> bought</Text></View>}
+        data={items}
         ItemSeparatorComponent={listSeparator}
       />
     </View>
