a25d2f8ece73d98fbd38223ac1a8d2812b171f18
jefishub <jere.manni@gmail.com>
2021-12-05 15:12:23 +0200

    base code

--- App.js
+++ App.js
@@ -1,78 +1,60 @@
-import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, TextInput, View, Button, FlatList } from 'react-native';
-import { initializeApp } from 'firebase/app';
-import { getDatabase, push, ref, onValue, removeChild, child  } from "firebase/database";
+import React, { useState, useEffect, useRef } from 'react';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
+import { Camera } from 'expo-camera';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCtBHGEjnXa6s622TNFKLJbBEYhNFE3xRc",
-  authDomain: "exercise-shoppinglist.firebaseapp.com",
-  databaseURL: "https://exercise-shoppinglist-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "exercise-shoppinglist",
-  storageBucket: "exercise-shoppinglist.appspot.com",
-  messagingSenderId: "301876490657",
-  appId: "1:301876490657:web:d9656e98ff987e52e11be0",
-  measurementId: "G-67NXFDKSYS"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
 
 export default function App() {
-  const [amount, setAmount] = useState('');
-  const [product, setProduct] = useState('');
-  const [items, setItems] = useState([]);
+  const [hasCameraPermission, setPermission] = useState(null);
+  const [photoName, setPhotoName] = useState('');
+  const [photoBase64, setPhotoBase64] = useState('');
+
+  const camera = useRef(null);
 
   useEffect(() => {
-    const itemsRef = ref(database, 'items/');
-    onValue(itemsRef, (snapshot) => {
-      console.log('test');
-      const data = snapshot.val();
-      setItems(Object.values(data));
-    })
+    askCameraPermission();
   }, []);
 
-  // Save shopping_list
-  const saveItem = () => {
-    push(ref(database, 'items/'),
-      { 'product': product, 'amount': amount });
+  const askCameraPermission = async () => {
+    const { status } = await Camera.requestCameraPermissionsAsync();
+    setPermission(status == 'granted');
   }
 
-  const listSeparator = () => {
-    return (
-      <View
-        style={{
-          height: 5,
-          width: "80%",
-          backgroundColor: "#fff",
-          marginLeft: "10%"
-        }}
-      />
-    );
+  const snap = async () => {
+    if (camera) {
+      const photo = await camera.current.takePictureAsync({ base64: true });
+      setPhotoName(photo.uri);
+      setPhotoBase64(photo.base64);
+    }
   };
 
   return (
-    <View style={styles.container}>
-      <TextInput placeholder='Product' style={{ marginTop: 30, fontSize: 18, width: 200, borderColor: 'gray', borderWidth: 1 }}
-        onChangeText={(product) => setProduct(product)}
-        value={product} />
-      <TextInput placeholder='Amounts' style={{ marginTop: 5, marginBottom: 5, fontSize: 18, width: 200, borderColor: 'gray', borderWidth: 1 }}
-        onChangeText={(amount) => setAmount(amount)}
-        value={amount} />
-      <Button onPress={saveItem} title="Save" />
-      <Text style={{ marginTop: 30, fontSize: 20 }}>Items</Text>
-      <FlatList
-        style={{ marginLeft: "5%" }}
-        keyExtractor={item => item.key}
-        renderItem={({ item }) => 
-        <View style={styles.listcontainer}>
-          <Text style={{ fontSize: 18 }}>{item.product}, {item.amount}</Text>
-        </View>}
-        data={items}
-        ItemSeparatorComponent={listSeparator}
-      />
+    <View style={{ flex: 1 }}>
+      {hasCameraPermission ?
+        (
+          <View style={{ flex: 1 }}>
+            <Camera style={{ flex: 4 }} ref={camera} />
+            <View>
+              <Button title="Take Photo" onPress={snap} />
+            </View>
+            <View style={{ flex: 4 }}>
+              {
+                photoName
+                  ? <Image style={{ flex: 1 }} source={{ uri: photoName }} />
+                  : <Text style={{ flex: 1 }}>File</Text>
+              }
+              {
+                photoBase64
+                  ? <Image style={{ flex: 1 }} source={{ uri: `data:image/gif;base64,${photoBase64}` }} />
+                  : <Text style={{ flex: 1 }} >Base64 encoded picture</Text>
+              }
+            </View>
+          </View>
+        ) : (
+          <Text>No access to camera</Text>
+        )}
     </View>
   );
+
 }
 
 const styles = StyleSheet.create({
@@ -81,10 +63,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  listcontainer: {
-    flexDirection: 'row',
-    backgroundColor: '#fff',
-    alignItems: 'center'
   },
 });
