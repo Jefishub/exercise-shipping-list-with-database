d35aa5ce094fdd558bbc013fe52f7042eb0d8294
jefishub <jere.manni@gmail.com>
2021-12-05 16:18:50 +0200

    works in expo

--- App.js
+++ App.js
@@ -1,67 +1,79 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { StyleSheet, Text, View, Button, Image } from 'react-native';
-import { Camera } from 'expo-camera';
-
+import { StatusBar } from 'expo-status-bar';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import * as Contacts from 'expo-contacts';
+import { FlatList } from 'react-native-gesture-handler';
 
 export default function App() {
-  const [hasCameraPermission, setPermission] = useState(null);
-  const [photoName, setPhotoName] = useState('');
-  const [photoBase64, setPhotoBase64] = useState('');
-
-  const camera = useRef(null);
+  const [hasPermission, setHasPermission] = useState(null);
+  const [contacts, setContacts] = useState([]);
 
   useEffect(() => {
-    askCameraPermission();
+    requestPermission();
   }, []);
 
-  const askCameraPermission = async () => {
-    const { status } = await Camera.requestCameraPermissionsAsync();
-    setPermission(status == 'granted');
+  const requestPermission = async () => {
+    const { status } = await Contacts.requestPermissionsAsync();
+    setHasPermission(status === 'granted');
   }
 
-  const snap = async () => {
-    if (camera) {
-      const photo = await camera.current.takePictureAsync({ base64: true });
-      setPhotoName(photo.uri);
-      setPhotoBase64(photo.base64);
+  const getContacts = async () => {
+    const { status } = await Contacts.requestPermissionsAsync();
+    setHasPermission(status === 'granted');
+
+    if (status === 'granted') {
+      const { data } = await Contacts.getContactsAsync({
+        fields: [Contacts.Fields.PhoneNumbers]
+      })
+
+      setContacts(data);
+      if (data.length > 0) {
+        setCurrentContact(data);
+      }
     }
-  };
+  }
 
   return (
-    <View style={{ flex: 1 }}>
-      {hasCameraPermission ?
-        (
-          <View style={{ flex: 1 }}>
-            <Camera style={{ flex: 4 }} ref={camera} />
-            <View>
-              <Button title="Take Photo" onPress={snap} />
-            </View>
-            <View style={{ flex: 4 }}>
-              {
-                photoName
-                  ? <Image style={{ flex: 1 }} source={{ uri: photoName }} />
-                  : <Text style={{ flex: 1 }}>File</Text>
-              }
-              {
-                photoBase64
-                  ? <Image style={{ flex: 1 }} source={{ uri: `data:image/gif;base64,${photoBase64}` }} />
-                  : <Text style={{ flex: 1 }} >Base64 encoded picture</Text>
-              }
-            </View>
+
+    <View style={styles.container} >
+      <StatusBar style="auto" />
+      {
+        hasPermission ? (
+          <View>
+            <FlatList
+              style={{ marginLeft: "5%" }}
+              keyExtractor={(item, index) => index}
+              renderItem={({ item }) =>
+                <View style={styles.listcontainer}>
+                  <Text style={{ fontSize: 18 }}>{item.firstName} {item.lastName}, {item.phoneNumbers[0].number}</Text>
+                </View>}
+              data={contacts}
+            />
+            <Button title="Show Contacts" onPress={getContacts} />
           </View>
         ) : (
-          <Text>No access to camera</Text>
-        )}
+          <View>
+            <Text>No permission to use Contacts</Text>
+            <Button title="Change permissions" onPress={requestPermission} />
+          </View>
+        )
+      }
     </View>
   );
-
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 36,
+    marginBottom: 36,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  listcontainer: {
+    flexDirection: 'row',
+    backgroundColor: '#fff',
+    alignItems: 'center'
   },
 });
